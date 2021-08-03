48f93f59702c2e6991e29d1effce74c5988af25e
Alexis190392 <alexis19031992@gmail.com>
2021-08-02 21:15:40 -0300

    creamos la query de busqueda para libro

--- src/main/java/com/libreria/libreria/repositories/LibroRepository.java
+++ src/main/java/com/libreria/libreria/repositories/LibroRepository.java
@@ -1,10 +1,24 @@
 package com.libreria.libreria.repositories;
 
 import com.libreria.libreria.entidades.Libro;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LibroRepository extends JpaRepository<Libro,String>{
+    
+    
+    //creo la query de busqueda de libro
+    @Query("select l from Libro l where "
+            + "l.isbn LIKE :query or "
+            + "l.titulo LIKE :query or "
+            + "l.anio LIKE :query or "
+            + "l.autor LIKE :query or"
+            + "l.editorial LIKE :query")
+    List<Libro> findByQuery(@Param("query") String query);
+    
     
 }
