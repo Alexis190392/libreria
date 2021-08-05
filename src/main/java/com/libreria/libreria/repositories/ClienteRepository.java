1943604216c6d657aea9c0dbabc7f81210c59d53
lumalnis <83096790+lumalnis@users.noreply.github.com>
2021-08-04 21:18:16 -0300

    Update ClienteRepository.java
    Cliente repositorio -Lu

--- src/main/java/com/libreria/libreria/repositories/ClienteRepository.java
+++ src/main/java/com/libreria/libreria/repositories/ClienteRepository.java
@@ -1,10 +1,22 @@
 package com.libreria.libreria.repositories;
 
 import com.libreria.libreria.entidades.Cliente;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente,String>{
+public interface ClienteRepository extends JpaRepository<Cliente,Long>{
+    
+    //Query de busqueda del cliente
+    @Query("select c from Cliente c where "
+            + "c.documento = :queryDni or "
+            + "c.nombre LIKE :query or "
+            + "c.apellido LIKE :query or "
+            + "c.domicilio LIKE :query or "
+            + "c.telefono LIKE :query")
+    List<Cliente> findByQuery(@Param("query") String query, @Param("queryDni") Long queryDni);
     
 }
