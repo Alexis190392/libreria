73e8b1be322b517877e6c6c01926ddbd0dba9650
lumalnis <83096790+lumalnis@users.noreply.github.com>
2021-08-04 21:17:36 -0300

    Update ClienteServicio.java
    Cliente Servicio actualizado - Lu

--- src/main/java/com/libreria/libreria/services/ClienteServicio.java
+++ src/main/java/com/libreria/libreria/services/ClienteServicio.java
@@ -3,36 +3,72 @@ package com.libreria.libreria.services;
 import com.libreria.libreria.entidades.Cliente;
 import com.libreria.libreria.repositories.ClienteRepository;
 import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteServicio {
-    
+
     @Autowired
     private ClienteRepository cr;
-    
-    public List<Cliente> listAll(){
-        return cr.findAll();
+
+    //guardar
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        return cr.save(cliente);
     }
-    
-    public Cliente save(Cliente c){
-        return cr.save(c);
-    }
-    
-    public Cliente save(Long dni, String nombre, String apellido, String domicilio, String tel){
+
+    @Transactional
+    public Cliente save(Long dni, String nombre, String apellido, String domicilio, String telefono) {
         Cliente c = new Cliente();
         c.setDocumento(dni);
         c.setNombre(nombre);
         c.setApellido(apellido);
         c.setDomicilio(domicilio);
-        c.setTelefono(tel);
+        c.setTelefono(telefono);
         return cr.save(c);
     }
-    
-    public void delete(Cliente c){
-        if(cr.findById(c.getDocumento().toString()).isPresent()){
-            cr.delete(c);
+
+    //eliminar
+    public void delete(Cliente cliente) {
+        cr.delete(cliente);
+    }
+
+    //eliminar by id
+    public void deleteById(Long dni) {
+        Optional<Cliente> op = cr.findById(dni);
+        if (op.isPresent()) {
+            cr.delete(op.get());
         }
+    }
+
+    //listar 
+    public List<Cliente> listAll() {
+        return cr.findAll();
+    }
+
+    //buscar
+    public List<Cliente> findByQuery(String query) {
+
+        Long dni;
+        try {
+            dni = Long.parseLong(query);
+        } catch (Exception e) {
+            dni = null;
+        }
+
+        return cr.findByQuery("%" + query + "%", dni);
+    }
+
+    //buscar por ID
+    public Cliente findById(Long dni) {
+        Cliente cliente = null;
+        Optional<Cliente> op = cr.findById(dni);
+        if (op.isPresent()) {
+            cliente = op.get();
+        }
+        return cliente;
     }
 }
