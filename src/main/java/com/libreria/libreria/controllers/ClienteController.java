378119fc8a3c2c1ace97d0dd7e4b157925f34b84
Alexis190392 <alexis19031992@gmail.com>
2021-08-04 21:59:49 -0300

    Reestructuracion codigo cliente

--- src/main/java/com/libreria/libreria/controllers/ClienteController.java
+++ src/main/java/com/libreria/libreria/controllers/ClienteController.java
@@ -1,44 +1,107 @@
+//package com.libreria.libreria.controllers;
+//
+//import com.libreria.libreria.services.ClienteServicio;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//@Controller
+//@RequestMapping("/cliente") //el principal del controller 
+//public class ClienteController {
+//
+//    @Autowired
+//    private ClienteServicio cs;
+//    
+//
+//    @GetMapping("/list") //metodos del controller
+//    public String listarClientes(Model m) {
+//        m.addAttribute("clientes", cs.listAll());
+//        return "administrarClientes";
+//    }
+//
+//    @GetMapping("/form")
+//    public String crearCliente() {
+//        return "crearCliente";
+//    }
+//
+//    @PostMapping("/save")
+//    public String guardarCliente(@RequestParam Long documento, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String domicilio, @RequestParam String telefono) {
+//        cs.save(documento, nombre, apellido, domicilio, telefono);
+//            return "redirect:/cliente/list";
+//    }
+//    
+//
+//    
+//    @GetMapping("/delete")
+//    public String eliminarCliente(@RequestParam(required=true) Long documento){
+//    cs.deleteById(documento);
+//    return"redirect:/cliente/list";
+//    }
+
 package com.libreria.libreria.controllers;
 
+import com.libreria.libreria.entidades.Cliente;
 import com.libreria.libreria.services.ClienteServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/cliente") //el principal del controller 
+@RequestMapping("/cliente")
 public class ClienteController {
-
+    
     @Autowired
     private ClienteServicio cs;
     
-
-    @GetMapping("/list") //metodos del controller
-    public String listarClientes(Model m) {
-        m.addAttribute("clientes", cs.listAll());
-        return "administrarClientes";
-    }
-
     @GetMapping("/form")
-    public String crearCliente() {
+    public String crearCliente(Model model, @RequestParam(required= false) String documento){
+        if(documento != null){
+            Cliente l = cs.findById(Long.parseLong(documento));
+            if(l != null){
+                model.addAttribute("cliente", l);
+            } else{
+                return "redirect:/cliente/list";
+            }
+        } else{
+            model.addAttribute("cliente",new Cliente());
+        }
         return "crearCliente";
     }
-
-    @PostMapping("/save")
-    public String guardarCliente(@RequestParam Long dni, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String domicilio, @RequestParam String tel) {
-        cs.save(dni, nombre, apellido, domicilio, tel);
-            return "redirect:/cliente/list";
+    
+    @GetMapping("/list")
+    public String listarClientes(Model model, @RequestParam(required= false) String query){
+        if(query != null){
+            model.addAttribute("clientes", cs.findByQuery(query));
+        } else {
+            model.addAttribute("clientes", cs.listAll());
+        }
+        return "administrarClientes";
     }
     
-
+    @PostMapping("/save")
+    public String guardarCliente(RedirectAttributes redat, @ModelAttribute Cliente cliente){
+        try {
+            cs.save(cliente);
+        } catch (Exception e) {
+            redat.addFlashAttribute("error", e.getMessage()); //mandando el mensaje de error a donde es redireccionado
+        }
+        return "redirect:/cliente/list";
+    }
     
+    //para eliminar
     @GetMapping("/delete")
-    public String eliminarCliente(@RequestParam(required=true) Long dni){
-    cs.deleteById(dni);
-    return"redirect:/cliente/list";
+    public String eliminarCliente(@RequestParam(required=true) Long documento){
+        cs.deleteByDocumento(documento); //desde cliente servicio permite la eliminacion
+        return "redirect:/cliente/list";
     }
 }
--- src/main/java/com/libreria/libreria/services/ClienteServicio.java
+++ src/main/java/com/libreria/libreria/services/ClienteServicio.java
@@ -8,67 +8,136 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+//@Service
+//public class ClienteServicio {
+//
+//    @Autowired
+//    private ClienteRepository cr;
+//
+//    //guardar
+//    @Transactional
+//    public Cliente save(Cliente cliente) {
+//        return cr.save(cliente);
+//    }
+//
+//    @Transactional
+//    public Cliente save(Long documento, String nombre, String apellido, String domicilio, String telefono) {
+//        Cliente c = new Cliente();
+//        c.setDocumento(documento);
+//        c.setNombre(nombre);
+//        c.setApellido(apellido);
+//        c.setDomicilio(domicilio);
+//        c.setTelefono(telefono);
+//        return cr.save(c);
+//    }
+//
+//    //eliminar
+//    public void delete(Cliente cliente) {
+//        cr.delete(cliente);
+//    }
+//
+//    //eliminar by id
+//    public void deleteById(Long documento) {
+//        Optional<Cliente> op = cr.findById(documento);
+//        if (op.isPresent()) {
+//            cr.delete(op.get());
+//        }
+//    }
+//
+//    //listar 
+//    public List<Cliente> listAll() {
+//        return cr.findAll();
+//    }
+//
+//    //buscar
+//    public List<Cliente> findByQuery(String query) {
+//
+//        Long documento;
+//        try {
+//            documento = Long.parseLong(query);
+//        } catch (Exception e) {
+//            documento = null;
+//        }
+//
+//        return cr.findByQuery("%" + query + "%", documento);
+//    }
+//
+//    //buscar por ID
+//    public Cliente findById(Long documento) {
+//        Cliente cliente = null;
+//        Optional<Cliente> op = cr.findById(documento);
+//        if (op.isPresent()) {
+//            cliente = op.get();
+//        }
+//        return cliente;
+//    }
+//}
+
 @Service
 public class ClienteServicio {
-
+    
     @Autowired
     private ClienteRepository cr;
-
+//    @Autowired
+//    private AutorServicio as;
+//    @Autowired
+//    private EditorialServicio es;
+    
+    
+    
     //guardar
     @Transactional
-    public Cliente save(Cliente cliente) {
+    public Cliente save(Cliente cliente){
         return cr.save(cliente);
     }
-
-    @Transactional
-    public Cliente save(Long dni, String nombre, String apellido, String domicilio, String telefono) {
+    
+  @Transactional
+    public Cliente save(Long documento, String nombre, String apellido, String domicilio, String telefono) {
         Cliente c = new Cliente();
-        c.setDocumento(dni);
+        c.setDocumento(documento);
         c.setNombre(nombre);
         c.setApellido(apellido);
         c.setDomicilio(domicilio);
         c.setTelefono(telefono);
         return cr.save(c);
     }
-
-    //eliminar
-    public void delete(Cliente cliente) {
-        cr.delete(cliente);
+    
+    
+    public List<Cliente> listAll(){
+        return cr.findAll(); 
     }
-
-    //eliminar by id
-    public void deleteById(Long dni) {
-        Optional<Cliente> op = cr.findById(dni);
-        if (op.isPresent()) {
-            cr.delete(op.get());
+    
+    //buscador general de clientes en la db
+    public List<Cliente> findByQuery(String query){
+        
+        Long documento;
+        try{
+            documento = Long.parseLong(query);   
+        } catch (Exception e){
+            documento= null;
         }
+        return cr.findByQuery("%"+query+"%", documento);
     }
-
-    //listar 
-    public List<Cliente> listAll() {
-        return cr.findAll();
-    }
-
-    //buscar
-    public List<Cliente> findByQuery(String query) {
-
-        Long dni;
-        try {
-            dni = Long.parseLong(query);
-        } catch (Exception e) {
-            dni = null;
-        }
-
-        return cr.findByQuery("%" + query + "%", dni);
-    }
-
-    //buscar por ID
-    public Cliente findById(Long dni) {
-        Cliente cliente = null;
-        Optional<Cliente> op = cr.findById(dni);
-        if (op.isPresent()) {
+    
+    
+    public Cliente findById(Long documento){
+        Cliente cliente= null;
+        Optional<Cliente> op = cr.findById(documento);
+        if(op.isPresent()){
             cliente = op.get();
         }
         return cliente;
     }
+    
+    public void delete (Cliente cliente){
+        cr.delete(cliente);
+    }
+    
+    public void deleteByDocumento(Long documento){
+        Optional<Cliente> op = cr.findById(documento);
+        if(op.isPresent()){
+            cr.delete(op.get()); //nos trae como objeto para eliminarlo
+        }
+    }
+
 }
