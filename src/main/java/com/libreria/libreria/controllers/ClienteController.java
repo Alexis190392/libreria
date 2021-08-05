31b04e2f8daf7a76c62e1476b2b3f70100da1364
lumalnis <83096790+lumalnis@users.noreply.github.com>
2021-08-04 21:18:58 -0300

    Update ClienteController.java
    te odio git...

--- src/main/java/com/libreria/libreria/controllers/ClienteController.java
+++ src/main/java/com/libreria/libreria/controllers/ClienteController.java
@@ -10,26 +10,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/cliente")
+@RequestMapping("/cliente") //el principal del controller 
 public class ClienteController {
-    
+
     @Autowired
     private ClienteServicio cs;
     
-    @GetMapping("/list")
-    public String listarClientes(Model m){
+
+    @GetMapping("/list") //metodos del controller
+    public String listarClientes(Model m) {
         m.addAttribute("clientes", cs.listAll());
         return "administrarClientes";
     }
-    
+
     @GetMapping("/form")
-    public String crearCliente(){
+    public String crearCliente() {
         return "crearCliente";
     }
-    
+
     @PostMapping("/save")
-    public String guardarCliente(@RequestParam Long dni, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String domicilio, @RequestParam String tel){
+    public String guardarCliente(@RequestParam Long dni, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String domicilio, @RequestParam String tel) {
         cs.save(dni, nombre, apellido, domicilio, tel);
-        return "redirect:/cliente/list";
+            return "redirect:/cliente/list";
+    }
+    
+
+    
+    @GetMapping("/delete")
+    public String eliminarCliente(@RequestParam(required=true) Long dni){
+    cs.deleteById(dni);
+    return"redirect:/cliente/list";
     }
 }
